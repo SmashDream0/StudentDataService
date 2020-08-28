cbadb972fb289f6f82b9cd95e8f47dc6cc9238f3
Павел Круглов <kruglov@megatec.ru>
2020-08-29 01:55:03 +0400

    test(StudentDataService.Test) Добавляю проект тестирования и его обвязку

---
+++ StudentDataService.Test/Fabric/EntityContextMockFactory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using StudentDataService.Entity.Context;
+
+namespace StudentDataService.Test.Fabric
+{
+    public class EntityContextMockFactory
+    {
+        /// <summary>
+        /// Очистить тестовую БД
+        /// </summary>
+        public void ClearData<TEntityContext>(String dbName)
+            where TEntityContext : DbContext, IEntityContext
+        {
+            var context = Create<TEntityContext>(dbName);
+
+            //зачищаю данные БД, т.к. данные в памяти статичны
+            //в то же время создание экземпляра данных - InMemoryDatabaseRoot приводит к тормозам, при каждом создании контекста, через этот метод
+            (context as DbContext).Database.EnsureDeleted();
+        }
+
+        public IEntityContext CreateWithCleanData<TEntityContext>(String dbName)
+            where TEntityContext : DbContext, IEntityContext
+        {
+            var context = Create<TEntityContext>(dbName);
+
+            //зачищаю данные БД, т.к. данные в памяти статичны
+            //в то же время создание экземпляра данных - InMemoryDatabaseRoot приводит к тормозам, при каждом создании контекста, через этот метод
+            (context as DbContext).Database.EnsureDeleted();
+
+            return context;
+        }
+
+        /// <summary>
+        /// Создать контекст с уникальным хранилище данных
+        /// </summary>
+        /// <typeparam name="TEntityContext"></typeparam>
+        /// <param name="dbName"></param>
+        /// <returns></returns>
+        public IEntityContext CreateWithSeparateData<TEntityContext>(String dbName)
+            where TEntityContext : DbContext, IEntityContext
+        {
+            var options = new DbContextOptionsBuilder<TEntityContext>()
+            .UseInMemoryDatabase(dbName, new InMemoryDatabaseRoot())
+            .Options;
+
+            var instance = Activator.CreateInstance(typeof(TEntityContext), options);
+
+            var context = instance as DbContext;
+
+            return context as TEntityContext;
+        }
+
+        /// <summary>
+        /// Создать контекст с разделяемым хранилищем данных
+        /// </summary>
+        /// <typeparam name="TEntityContext"></typeparam>
+        /// <param name="dbName"></param>
+        /// <returns></returns>
+        public IEntityContext Create<TEntityContext>(String dbName)
+            where TEntityContext : DbContext, IEntityContext
+        {
+            var options = new DbContextOptionsBuilder<TEntityContext>()
+            .UseInMemoryDatabase(dbName)
+            .Options;
+
+            var instance = Activator.CreateInstance(typeof(TEntityContext), options);
+
+            var context = instance as DbContext;
+
+            return context as TEntityContext;
+        }
+    }
+}
---
+++ StudentDataService.Test/Fabric/MockFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StudentDataService.Entity.Context;
+
+namespace StudentDataService.Test.Fabric
+{
+    public class MockFactory
+    {
+        /// <summary>
+        /// Получить контекст с уникальным хранилищем данных
+        /// </summary>
+        /// <returns></returns>
+        public IEntityContext GetContextSeparate()
+        { return new EntityContextMockFactory().CreateWithSeparateData<EntityContext>("Test"); }
+
+        /// <summary>
+        /// Получить контекст с разделяемым хранилищем данных
+        /// </summary>
+        /// <returns></returns>
+        public IEntityContext GetContext()
+        { return new EntityContextMockFactory().Create<EntityContext>("Test"); }
+
+        /// <summary>
+        /// Очистить разделяемое хранилище данных
+        /// </summary>
+        public void Clear()
+        { new EntityContextMockFactory().ClearData<EntityContext>("Test"); }
+    }
+}
