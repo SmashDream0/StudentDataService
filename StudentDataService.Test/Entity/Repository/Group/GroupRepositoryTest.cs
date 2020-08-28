ca20c1317343a80846967edb78dbfda80b4afc44
Павел Круглов <kruglov@megatec.ru>
2020-08-29 01:55:11 +0400

    improve(GroupRepositoryTest) Добавляю тесты репозитария

---
+++ StudentDataService.Test/Entity/Repository/Group/GroupRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using StudentDataService.Entity.POCO;
+using StudentDataService.Entity.Repository.Group;
+using StudentDataService.Test.Fabric;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace StudentDataService.Test.Entity.Repository.Group
+{
+    public class GroupRepositoryTest
+    {
+        [Fact]
+        public void FindByNameExist()
+        {
+            var context = new MockFactory().GetContextSeparate();
+
+            var target = new GroupEntity() { Name = "12" };
+            context.Set<GroupEntity>().AddRange(new[] { target, new GroupEntity() { Name = "123" } });
+            context.SaveChanges();
+
+            var repository = new GroupRepository(context);
+
+            var result = repository.Find("12");
+
+            Assert.Equal(target.Key, result.First().Key);
+        }
+
+        [Fact]
+        public void FindByNameNotExist()
+        {
+            var context = new MockFactory().GetContextSeparate();
+
+            var target = new GroupEntity() { Name = "12" };
+            context.Set<GroupEntity>().AddRange(new[] { target, new GroupEntity() { Name = "123" } });
+            context.SaveChanges();
+
+            var repository = new GroupRepository(context);
+
+            var result = repository.Find("1");
+
+            Assert.Empty(result);
+        }
+    }
+}
